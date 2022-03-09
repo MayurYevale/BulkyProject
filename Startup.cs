cdd3dde7abd8ccc36e1828813296f2b2f242f237
Mayur Yevale <mayur_yevale@persistent.com>
2022-03-09 10:04:16 +0530

    Created three sub projects Utility, DataAccess, Models

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace BulkyBook
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
@@ -60,7 +60,7 @@ namespace BulkyBook
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {
+            {       // if nothing given in url then it will show home/index page as below
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
